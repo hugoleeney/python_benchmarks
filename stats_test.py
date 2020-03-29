f85de9ff24237a03ed22e1b70ddbc403dc0485a1
hugoleeney <62838185+hugoleeney@users.noreply.github.com>
2020-03-30 00:40:44 +0100

    Update stats_test.py

--- stats_test.py
+++ stats_test.py
@@ -1,45 +1,58 @@
 #!/usr/bin/env python3
 """
-Written to work with standard python 3.5.5 (no additional libraries required).
+Written to work with standard python 3.5.5 (external libraries required: numpy==1.18.2).
 Explores the fastest way to run simple statistics on arrays of numbers.
-
-Summary Conclusion: for non-trivial cases it is quicker to create a numpy array and use numpy functions.
+Summary Conclusion: for non-trivial cases it is quicker to create a numpy array and use numpy functions. 
+You can run numpy functions on python lists but it would seem you have the same overhead of converting to numpy array.
+There is no advantage gained when running numpy functions more than once.
 
 Sample Output:
-
 timing: create array
-runtime=32.42217493057251
+runtime=4.32141900062561
 
 timing: copy array
-runtime=0.014689207077026367
+runtime=0.016638994216918945
 
 timing: create numpy array
-runtime=0.08298778533935547
+runtime=0.09427499771118164
 
 timing: python min
 python_min=0
-runtime=0.019994020462036133
+runtime=0.025493144989013672
 
 timing: statistics mean
-statistics_mean=50041.906475
-runtime=10.625783920288086
+statistics_mean=49951.713239
+runtime=1.287816047668457
 
 timing: statistics stddev
-statistics_stdev=28843.027183552997
-runtime=55.764643907547
+statistics_stdev=28862.75487516741
+runtime=5.2155139446258545
+
+timing: numpy min on python
+numpy_min=0
+runtime=0.10659909248352051
+
+timing: numpy mean on python
+49951.713239
+runtime=0.1568741798400879
+
+timing: numpy stddev on python
+28862.740443786366
+runtime=0.12770605087280273
 
 timing: numpy min
 numpy_min=0
-runtime=0.006093025207519531
+runtime=0.002084016799926758
 
 timing: numpy mean
-50041.906475
-runtime=0.0033330917358398438
+49951.713239
+runtime=0.001786947250366211
 
 timing: numpy stddev
-28843.012762035803
-runtime=0.00748896598815918
+28862.740443786366
+runtime=0.010833978652954102
 """
+
 
 import random
 import statistics
@@ -84,6 +97,22 @@ with timer("statistics stddev"):
     statistics_stdev = statistics.stdev(python_a)
     print("statistics_stdev=%s"%statistics_stdev)
 
+
+with timer("numpy min on python"):
+    numpy_min = np.min(python_a)
+    print("numpy_min=%s"%numpy_min)
+
+with timer("numpy mean on python"):
+    numpy_mean = np.mean(python_a)
+    print(numpy_mean)
+
+with timer("numpy stddev on python"):
+    numpy_stddev = np.std(python_a)
+    print(numpy_stddev)
+
+
+numpy_a = np.array(python_a)
+
 with timer("numpy min"):
     numpy_min = np.min(numpy_a)
     print("numpy_min=%s"%numpy_min)
@@ -100,7 +129,12 @@ with timer("numpy stddev"):
     numpy_stddev = np.std(numpy_a)
     print(numpy_stddev)
 
+with timer("numpy stddev again on same array"):
+    numpy_stddev = np.std(numpy_a)
+    print(numpy_stddev)
+
 
 assert math.isclose(statistics_mean, numpy_mean, abs_tol=1), "%s %s"%(statistics_mean, numpy_mean)
 
 assert math.isclose(statistics_stdev, numpy_stddev, abs_tol=1), "%s %s"%(statistics_stdev, numpy_stddev)
+
